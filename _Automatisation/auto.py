0507f735e84d740323336945b2b8c96ade2136a8
“lencshu <lencshu@gmail.com>
2017-12-12 18:44:25 +0100

    Bug Fixed

--- _Automatisation/auto.py
+++ _Automatisation/auto.py
@@ -60,7 +60,7 @@ else :
 	conf.set("HTML", "DeleteOriginHtml",deleteOriginHtml)
 	conf.set("TimeTag", "IniModifiedTime",deleteOriginHtml)
 	conf.set("MultiMedia", "DirName",multiMediaDir) # 获取指定section 的option值
-	# conf.set("MultiMedia", "LastChangeTime",lastChangeTime )
+	conf.set("MultiMedia", "LastChangeTime",lastChangeTime )
 	timeTag=time.strftime("%Y-%m-%d %H:%M:%S %a", time.localtime())
 	conf.set("TimeTag", "IniModifiedTime",timeTag)
 	conf.write(open('auto.ini', 'w'))
@@ -76,6 +76,8 @@ print lastChangeTime
 refTimeAfterAll = lastChangeTime
 ChangedTime=0
 pngChangedSigne=0
+htmlOriginalExiste = os.path.exists(parentDir+"\\"+ htmlName)
+# pngExiste=0
 
 def iniConfPng(t):
 	conf.read('auto.ini')
@@ -111,7 +113,7 @@ for file in os.listdir(mediaFolder):
 		elif lastChangeTime == 0:
 			if ChangedTime < fileTime:
 				ChangedTime = fileTime
-			pngChangedSigne=2
+				pngChangedSigne=2
 
 if pngChangedSigne==1:
 	iniConfPng(refTimeAfterAll)
@@ -119,16 +121,20 @@ if pngChangedSigne==1:
 if pngChangedSigne==2:
 	iniConfPng(ChangedTime)
 	print "==== PNGs initialised modified date created "
+# if pngChangedSigne==3:
+# 	iniConfPng(0)
+# 	print "==== No PNGs found in the mediafolder "
+
 
 ##
 ## 网页重制并加入sidebar
 ##
 
-htmlExiste = os.path.exists(parentDir+"\\index.html")
+indexExiste = os.path.exists(parentDir+"\\index.html")
 htmlOriginalPath = parentDir + "\\" + htmlName
 indexPath = parentDir + "\\index.html"
 
-if htmlExiste:
+if indexExiste and htmlOriginalExiste:
 	os.remove(indexPath)
 #========change the html file name======
 html = open(htmlOriginalPath,"r+")
